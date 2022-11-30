c8a8aa77304289b4d61c92f0daf48f0851746a54
dedalovitor <112886142+dedalovitor@users.noreply.github.com>
2022-11-30 12:28:11 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -26,8 +26,6 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
@@ -36,7 +34,7 @@ function render(variables = {}) {
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name ? variables.name : "Name"} ${variables.lastname ? variables.lastname : "Lastname"}</h1>
           <h2>${variables.role ? variables.role: "Role"}</h2>
-          <h3>${variables.city}, ${variables.country}</h3>
+          <h3>${variables.city ? variables.city : "Valencia"}, ${variables.country ? variables.country : "Spain"}</h3>
           <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
